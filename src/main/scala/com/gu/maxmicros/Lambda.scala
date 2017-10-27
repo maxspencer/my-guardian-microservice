5b433ec515a02f43d8b00a53d2a686e6271ad5f8
Max Spencer <max.spencer@guardian.co.uk>
2017-10-27 16:06:55 +0100

    Make lambda test for primality

--- src/main/scala/com/gu/maxmicros/Lambda.scala
+++ src/main/scala/com/gu/maxmicros/Lambda.scala
@@ -1,14 +1,21 @@
 package com.gu.maxmicros
 
-import java.io.{InputStream, OutputStream}
+import java.io.{DataInputStream, InputStream, OutputStream}
+
 import io.circe.syntax._
 import java.nio.charset.StandardCharsets.UTF_8
+import Math._
 
 object Lambda {
 
   def handler(in: InputStream, out: OutputStream) {
-    val response = LambdaResponse(200, Map("Content-Type" -> "application/json"), "Max says hello.")
+    val dis = new DataInputStream(in)
+    val number = dis.readInt()
+    val numberStatus = if (isPrime(number)) "this is a prime number" else "this is not a prime number"
+
+    val response = LambdaResponse(200, Map("Content-Type" -> "application/json"), numberStatus)
     out.write(response.asJson.noSpaces.getBytes(UTF_8))
   }
 
+  def isPrime(number: Int): Boolean = (0 to sqrt(number).toInt).forall(i => number % i != 0)
 }
