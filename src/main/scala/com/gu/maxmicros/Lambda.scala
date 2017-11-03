b70058f757387aa36848fa6bfe5021fc01701e27
Max Spencer <max.spencer@guardian.co.uk>
2017-11-03 15:02:21 +0000

    Debugging lambda

--- src/main/scala/com/gu/maxmicros/Lambda.scala
+++ src/main/scala/com/gu/maxmicros/Lambda.scala
@@ -6,16 +6,31 @@ import io.circe.syntax._
 import java.nio.charset.StandardCharsets.UTF_8
 import Math._
 
+import scala.util.{Failure, Success, Try}
+
 object Lambda {
 
   def handler(in: InputStream, out: OutputStream) {
-    val dis = new DataInputStream(in)
-    val number = dis.readInt()
-    val numberStatus = if (isPrime(number)) "this is a prime number" else "this is not a prime number"
-
-    val response = LambdaResponse(200, Map("Content-Type" -> "application/json"), numberStatus)
-    out.write(response.asJson.noSpaces.getBytes(UTF_8))
+    try {
+      val dis = new DataInputStream(in)
+      val bytes = new Array[Byte](2^7)
+      dis.readFully(bytes)
+      val string = new String(bytes)
+      /*
+      val response = Try(dis.readInt()) match {
+        case Success(number) => LambdaResponse(200, Map("Content-Type" -> "application/json"), primeStatus(number))
+        case Failure(exception) => LambdaResponse(500, Map.empty, exception.toString)
+      }
+      */
+      val response = LambdaResponse(200, Map("Content-Type" -> "text/plain"), string)
+      out.write(response.asJson.noSpaces.getBytes(UTF_8))
+    } finally {
+      in.close()
+      out.close()
+    }
   }
+
+  def primeStatus(number: Int): String = if (isPrime(number)) "this is a prime number" else "this is not a prime number"
 
   def isPrime(number: Int): Boolean = (2 to sqrt(number).toInt).forall(i => number % i != 0)
 }
